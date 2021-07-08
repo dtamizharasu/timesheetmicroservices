64bafc8c78efe9a6ed4238d8d44400849762ad53
Joshwin.Thomas <joshwin.thomas@ust.com>
2021-07-08 13:20:11 +0530

    committing changes by joshwin

--- src/main/java/com/timesheet/TimesheetmicroservicesApplication.java
+++ src/main/java/com/timesheet/TimesheetmicroservicesApplication.java
@@ -2,18 +2,20 @@ package com.timesheet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class TimesheetmicroservicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TimesheetmicroservicesApplication.class, args);
 	}
 
-//	@LoadBalanced
+	@LoadBalanced
 	@Bean
 	RestTemplate restTemplate(){
 		return new RestTemplate();
